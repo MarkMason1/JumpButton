82facee012bb95313639fa729e07bcb108011492
MarkMason1 <71779805+MarkMason1@users.noreply.github.com>
2021-06-21 14:50:56 +0300

    Add files via upload

---
+++ JumpingButton/JumpingButton/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+using System.Windows.Forms;
+
+namespace JumpingButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::JumpingButton.Properties.Resources.hqdefault;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(247, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 108);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "I am Speed";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImage = global::JumpingButton.Properties.Resources._81BXTSOLSUL__AC_SL1500_;
+            this.ClientSize = new System.Drawing.Size(1177, 599);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        
+    }
+}
+
---
+++ JumpingButton/JumpingButton/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace JumpingButton
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        
+        public static extern IntPtr LoadCursorFromFile(string filename);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+           /* Random rand = new Random();
+            var LocationY = rand.Next(ClientSize.Height-150);
+            var LocationX = rand.Next(ClientSize.Width-250);
+            button1.Location = new Point(LocationX, LocationY);*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+         
+            var buttonX = button1.Location.X ;
+            var buttonY = button1.Location.Y ;
+            var mouseX = Control.MousePosition.X;
+            var mouseY = Control.MousePosition.Y;
+
+            Text = $"Button: {buttonX}, {buttonY} // Mouse: {mouseX}, {mouseY}";
+            
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random rand = new Random();
+            var buttonX = button1.Location.X;
+            var buttonY = button1.Location.Y;
+            var mouseX = Control.MousePosition.X;
+            var mouseY = Control.MousePosition.Y;
+
+            Text = $"Button: {buttonX}, {buttonY} // Mouse: {mouseX}, {mouseY}";
+            var PointM = new Point(mouseX -250, mouseY-150);
+            var PointB = new Point(buttonX, buttonY);
+            if (PointB.Equals(button1.Location))
+            {
+                var LocationY = rand.Next(ClientSize.Height - 150);
+                var LocationX = rand.Next(ClientSize.Width - 250);
+                button1.Location = new Point(LocationX, LocationY);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Cursor mycursor = new Cursor(Cursor.Current.Handle);
+            
+            IntPtr colorcursorhandle = LoadCursorFromFile(@"C:\Users\mark2\source\repos\JumpingButton\JumpingButton\bin\Debug\police.cur");
+            mycursor.GetType().InvokeMember("handle", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetField, null, mycursor, new object[] { colorcursorhandle });
+            this.Cursor = mycursor;
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
